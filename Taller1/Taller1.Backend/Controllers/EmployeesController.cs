f3ad9f264e5efef8032b0a13cd4a215f794206af
Juan Alv <juanjo.alvarez.250@gmail.com>
2025-09-07 22:49:58 -0500

    Creation of Project, Db and basic controller

---
+++ Taller1/Taller1.Backend/Controllers/EmployeesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Taller1.Backend.Data;
+using Taller1.Shared.Entities;
+
+namespace Taller1.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public EmployeesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Employees.ToListAsync());
+    }
+
+    [HttpGet("{name}")]
+    public async Task<IActionResult> GetAsync(string name)
+    {
+        return Ok(await _context.Employees.Where
+            (x => x.FirstName.Contains(name) || x.LastName.Contains(name))
+            .ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Employee employee)
+    {
+        _context.Add(employee);
+        await _context.SaveChangesAsync();
+        return Ok(employee);
+    }
+}
---
+++ Taller1/Taller1.Backend/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taller1.Shared.Entities;
+
+namespace Taller1.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
