85f1fd78216b3f1e67969b0e24e51169578c9642
Juan Alv <juanjo.alvarez.250@gmail.com>
2025-10-06 23:12:47 -0500

    Add PaginationDTO, QueryableExtension Helper and Generic pagination

---
+++ Taller1/Taller1.Shared/DTOs/PaginationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Taller.Shared.DTOs;
+
+public class PaginationDTO
+{
+    public int Id { get; set; }
+    public int Page { get; set; } = 1;
+    public int RecordNumber { get; set; } = 10;
+}
