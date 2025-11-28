85847522ba0d28c7c6eeeedefe076ec10b203b32
Juan Alv <juanjo.alvarez.250@gmail.com>
2025-11-27 22:11:42 -0500

    Preparation of AuthenticationProviderJWT and delete AuthenticationProviderTest

--- Taller1/Taller.Frontend/Program.cs
+++ Taller1/Taller.Frontend/Program.cs
@@ -3,15 +3,22 @@ using MudBlazor.Services;
 using Taller.Frontend.AuthenticationProviders;
 using Taller.Frontend.Components;
 using Taller.Frontend.Repositories;
+using Taller.Frontend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMudServices();
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddSingleton(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7028") });
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x =>
+    x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x =>
+    x.GetRequiredService<AuthenticationProviderJWT>());
 
 var app = builder.Build();
 
