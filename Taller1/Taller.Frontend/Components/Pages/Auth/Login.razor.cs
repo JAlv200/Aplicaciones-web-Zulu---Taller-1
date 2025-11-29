18088a57f4037527e0f6daaf225fc7aad1843108
Juan Alv <juanjo.alvarez.250@gmail.com>
2025-11-29 12:11:23 -0500

    Login and Logout implemented and Changes in the Home menu message for authenticated users depending on their role

--- Taller1/Taller.Frontend/Components/Pages/Auth/Login.razor.cs
+++ Taller1/Taller.Frontend/Components/Pages/Auth/Login.razor.cs
@@ -1,5 +1,59 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Taller.Frontend.Repositories;
+using Taller.Frontend.Services;
+using Taller.Shared.DTOs;
+
 namespace Taller.Frontend.Components.Pages.Auth;
 
 public partial class Login
 {
+    private LoginDTO LoginDTO = new();
+    private bool WasClose;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IDialogService DialogService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private ILoginService LoginService { get; set; } = null!;
+
+    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
+
+    private void ShowModalResendConfirmationEmail()
+    {
+        var CloseOnEscapeKey = new DialogOptions() { CloseOnEscapeKey = true, CloseButton = true, MaxWidth = MaxWidth.ExtraLarge };
+        DialogService.ShowAsync<ResendConfirmationEmailToken>("Reenvio de Correo", CloseOnEscapeKey);
+    }
+
+    private void ShowModalRecoverPassword()
+    {
+        var CloseOnEscapeKey = new DialogOptions() { CloseOnEscapeKey = true, CloseButton = true, MaxWidth = MaxWidth.ExtraLarge };
+        DialogService.ShowAsync<RecoverPassword>("Recuperar contraseña", CloseOnEscapeKey);
+    }
+
+    private void CloseModal()
+    {
+        WasClose = true;
+        MudDialog.Cancel();
+    }
+
+    private async Task LoginAsync()
+    {
+        if (WasClose)
+        {
+            NavigationManager.NavigateTo("/");
+            return;
+        }
+
+        var responseHttp = await Repository.PostAsync<LoginDTO, TokenDTO>("/api/accounts/Login", LoginDTO);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(message!, Severity.Error);
+            return;
+        }
+
+        await LoginService.LoginAsync(responseHttp.Response!.Token);
+        NavigationManager.NavigateTo("/");
+    }
 }
